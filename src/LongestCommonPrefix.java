a7729d29e0c9049fa58461a4ee61d96d8f11c5c4
ShahriarXTahmid <shahriar2341@gmail.com>
2026-01-07 23:40:26 +0600

    Updated

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -1,3 +1,17 @@
 public class LongestCommonPrefix(String[] strs) {
+   if(strs == null || str.length() == 0 ){
+       return "";
+    }
 
+    String prefix = str[0];
+
+   for(int i = 0; i<prefix.length(); i++){
+       char ch = prefix.charAt(i);
+       for(int j = 1; j<str.length(); j++){
+           if(i== strs[j].length() || str[j].charAt(i) != ch){
+               return prefix.substring(0,i);
+           }
+        }
+    }
+   return prefix;
 }
