a84bc8239d6fd9c0fe2b721d68ab725f3b2a1586
ShahriarXTahmid <shahriar2341@gmail.com>
2026-01-08 13:45:52 +0600

    Uploaded

---
+++ src/ThreeSum.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums){
+
+      if(nums.length < 3){
+          return new List<List<Integer>>();
+      }
+
+      else{
+
+      }
+    }
+}
