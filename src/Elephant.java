044aa78c1dbc48f0b497cc9b248b067e28665aa1
ShahriarXTahmid <shahriar2341@gmail.com>
2026-01-20 02:44:59 +0600

    Committed

--- src/Elephant.java
+++ src/Elephant.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Elephant {
-   public static void main(String[] args) {
-
-
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        int steps = (x + 4) / 5;
+        System.out.print(steps);
     }
 }
