c5ca9c9e25b46cc0872351b6e60ab79d661b4321
ShahriarXTahmid <shahriar2341@gmail.com>
2026-01-21 01:42:28 +0600

    Committed

---
+++ src/SoldierAndBananas.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SoldierAndBananas {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int price = input.nextInt();
+        long dollar = input.nextLong();
+        int banana = input.nextInt();
+        long totalPrice = 0;
+
+        for (int i = 1; i <= banana; i++) {
+            int calc = price * i;
+            totalPrice += calc;
+        }
+        System.out.print(totalPrice - dollar);
+    }
+}
