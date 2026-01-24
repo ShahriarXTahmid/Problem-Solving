9fd4ec2180dd616c3027a7d63e0e0c74ce491a2a
ShahriarXTahmid <shahriar2341@gmail.com>
2026-01-25 01:00:06 +0600

    Committed

--- src/Tram.java
+++ src/Tram.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Tram {
-   public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int station = input.nextInt();
+        int a, b;
+        int count = 0;
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        while (station-- > 0) {
+            a = input.nextInt();
+            b = input.nextInt();
+
+            count = count + b;
+            count = count - a;
+
+            arrayList.add(count);
+        }
+
+        Collections.sort(arrayList);
+        System.out.print(arrayList.getLast());
 
     }
 }
