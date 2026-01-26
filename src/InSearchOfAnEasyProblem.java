9c459c7aac8307a4c5aa76067c53bdc227b2e95b
ShahriarXTahmid <shahriar2341@gmail.com>
2026-01-27 04:33:38 +0600

    Committed

---
+++ src/InSearchOfAnEasyProblem.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class InSearchOfAnEasyProblem {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int test = input.nextInt();
+        boolean flag = false;
+        int count = 0;
+
+        while (test-- > 0) {
+            count = input.nextInt();
+            if (count == 1) {
+                flag = true;
+                break;
+            }
+        }
+        if (flag) {
+            System.out.print("Hard");
+        } else {
+            System.out.print("Easy");
+        }
+    }
+}
